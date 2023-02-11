f73a4ffcd4fb502a2be8d3fbf1d0a2e46ae33cfc
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-02-11 18:15:28 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,121 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
+.card{
+  border-radius: 24px;
+  border: 3px solid #041E42;
+  padding: 8px;
+  width: 400px;
+  display: block;
+  margin: auto;
+  position: relative;
+  overflow: hidden;
+  box-shadow: 0px 8px 24px #999;
+  background-color: lightgrey;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.textbox{
+  font-size: 15px;
+  margin-left: 112px;
+  text-align: right;
+  border-radius: 24px;
+}
+.description{
+  text-shadow: -1px 1px 2px #FFFFFF,
+				  1px 1px 2px #FFFFFF,
+				  1px -1px 0 #FFFFFF,
+				  -1px -1px 0 #FFFFFF;
+}
+.haxbtn{
+  display:inline-block;
+  text-align: center;
+  color: white;
+  background-color: #041E42;
+  border-radius: 5px;
+  font-size: 20px;
+  width: 120px;
+  height: 32px;
+  box-shadow: 0px 10px 24px #999;
+  border: 1px solid #041E42;
+  visibility: visible;
+  padding-top: 10px;
+  margin-right: 32px;
+}
+.haxbtn:hover{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.haxbtn:focus{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.title{
+  color: white;
+  font-size: 24px;
+  text-shadow: -1px 1px 2px #000,
+				  1px 1px 2px #000,
+				  1px -1px 0 #000,
+				  -1px -1px 0 #000;
+}
+.img{
+  width: 200px;
+  border-radius: 24px;
+  box-shadow: 0px 10px 24px #999;
+  float: left;
+}
+.outsideBtn{
+  color: white;
+  background-color: #041E42;
+  border-radius: 5px;
+  font-size: 20px;
+  width: 120px;
+  height: 50px;
+  box-shadow: 0px 10px 24px #999;
+  border: 1px solid #041E42;
+  margin-right: 32px;
+  margin-bottom: 16px;
+  display: inline-block;
+}
+.outsideBtn:hover{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.outsideBtn:focus{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.basic{
+  background-color: hotpink;
+}
+.hoverCard{
+  border: 3px solid purple;
+  box-shadow: 0px 8px 24px yellow;
+}
+input:checked + label + .description { display:none; }
 </style>
