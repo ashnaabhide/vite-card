c33af5db37a31c252e239f62dfa4cedbd8b63d07
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-02-11 18:15:06 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,30 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="buttons">
+  <button class="duplicate">duplicate</button>
+  <button data-delete-btn>delete</button>
+  <button data-toggle-btn>toggle</button>
+  <button class="modify">modify title</button>
+</div>
+<div class="wrapper">
+  <div class="container">
+  <img class="image" src="https://static.boredpanda.com/blog/wp-content/uploads/2020/05/700-1.jpg" />
+  <div class="header">
+    <h3>BORZOI</h3>
+      <h4>woof</h4>
+    <details class="details">
+      <summary>Details</summary>
+      <div>
+        <ul>
+        <li>The magnificent borzoi! </li>
+        <li>Look how  disproportionate its nose is. Marvelous. </li>
+        </ul>
+      </div>
+  </details>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
+</div>
 </template>
 
 <style scoped>
